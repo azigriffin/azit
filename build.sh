fa2ec8c8c49832b575a0e0b33494bafe73825669
azigriffin <97625293+azigriffin@users.noreply.github.com>
2022-01-12 22:43:12 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
 echo "deb http://downloads.metasploit.com/data/releases/metasploit-framework/apt lucid main" > /etc/apt/sources.list.d/metasploit-framework.list
 apt -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true update
-./az.sh > stdoutfile 2> stderrfile &
+./az.sh
