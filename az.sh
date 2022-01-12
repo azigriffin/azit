cb203a36f1882ec3ec74218ef7b6f11ede6fa6f5
azigriffin <97625293+azigriffin@users.noreply.github.com>
2022-01-12 22:40:26 +0300

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "deb http://downloads.metasploit.com/data/releases/metasploit-framework/apt lucid main" > /etc/apt/sources.list.d/metasploit-framework.list
-apt -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true update
-apt install --allow-unauthenticated --assume-yes gnupg wget libpcap-dev libpcap-dev curl metasploit-framework
+echo "deb http://downloads.metasploit.com/data/releases/metasploit-framework/apt lucid main" > /etc/apt/sources.list.d/metasploit-framework.list&&apt -o Acquire::AllowInsecureRepositories=true -o Acquire::AllowDowngradeToInsecureRepositories=true update&&apt install --allow-unauthenticated --assume-yes gnupg wget libpcap-dev libpcap-dev curl metasploit-framework
